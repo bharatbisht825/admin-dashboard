2083bbeaf35555dec0675f79bfd4edcf0064e471
bharatbisht825 <bharatbisht825@gmail.com>
2024-06-24 20:22:43 +0530

    first

---
+++ src/pages/Charts/Line.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import {
+  ChartComponent,
+  SeriesCollectionDirective,
+  SeriesDirective,
+  Inject,
+  LineSeries,
+  DateTime,
+  Legend,
+  Tooltip,
+} from "@syncfusion/ej2-react-charts";
+
+import { Header } from "../../components";
+import { lineCustomSeries, LinePrimaryXAxis, LinePrimaryYAxis } from "../../data/dummy";
+import { useStateContext } from "../../contexts/ContextProvider";
+
+const Line = () => {
+  const { currentMode } = useStateContext();
+
+  return (
+    <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
+      <Header category="Chart" title="Inflation Rate" />
+      <div className="w-full">
+        <ChartComponent
+          id="line-chart"
+          height="420px"
+          primaryXAxis={LinePrimaryXAxis}
+          primaryYAxis={LinePrimaryYAxis}
+          chartArea={{ border: { width: 0 } }}
+          tooltip={{ enable: true }}
+          background={currentMode === "Dark" ? "#33373E" : "#fff"}
+          legendSettings={{ background: "white" }}
+        >
+          <Inject services={[LineSeries, DateTime, Legend, Tooltip]} />
+          <SeriesCollectionDirective>
+            {lineCustomSeries.map((item, index) => (
+              <SeriesDirective key={index} {...item} />
+            ))}
+          </SeriesCollectionDirective>
+        </ChartComponent>
+      </div>
+    </div>
+  );
+};
+
+export default Line;
